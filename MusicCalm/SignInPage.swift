64ac98a6f7a00b1f8ed619183914ac77ef59a458
Nithya Appannagaari <nappannagaari087@student.fuhsd.org>
2022-11-28 23:49:57 -0800

    pages

---
+++ MusicCalm/SignInPage.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  MusicCalm
+//
+//  Created by Nithya Appannagaari on 11/27/22.
+//
+
+import UIKit
+
+class SignInPage: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+   /** @IBAction func showMessage(sender: UIButton) {
+        
+        let selectedButton = sender
+        
+        let urMom = sender
+        
+        var emojiDict: [String: String] = ["😋": "yummy", "💗": "flashing heart", "🥶": "wassup bro", "🫦": "lip bite", "sign in": "enter your username"]
+        
+        if let wordToLookup = selectedButton.titleLabel?.text {
+            let meaning = emojiDict[wordToLookup]
+            
+            let alertController = UIAlertController(title:"Meaning", message: meaning, preferredStyle: UIAlertController.Style.alert)
+            alertController.addAction(UIAlertAction(title:"OK", style: UIAlertAction.Style.default, handler:nil))
+            present(alertController, animated: true, completion: nil)
+        }
+        
+    
+        
+    }**/
+
+
+}
+
